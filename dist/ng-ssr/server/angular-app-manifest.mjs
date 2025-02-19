59eabfbebbd0e5605f2c224b109e16fb2fdd03cb
Chhayhong <chhayhong@Chhayhongs-MacBook-Pro.local>
2025-02-19 15:34:37 +0700

    added new build

--- dist/ng-ssr/server/angular-app-manifest.mjs
+++ dist/ng-ssr/server/angular-app-manifest.mjs
@@ -21,8 +21,8 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 466, hash: '3400de5289023b5e68d94ae9c297cbed10084e87637f33cea1d7ab698c25bb07', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 979, hash: 'da05977e8c40b0824394621b6c9970343fcd7b601cca0e8308f9d56bc2ab1a5a', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 466, hash: 'b12233d941ffc25635f102c518ab315d1042854d531eaa344a6afe152e127d92', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 979, hash: 'c9d9546403a352a9d3c158f8d0c0f6dadf5ceb2fc4f90d3d2a615c418aee2605', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
