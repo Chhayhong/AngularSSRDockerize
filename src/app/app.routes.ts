ceb49c0e46d41582e0c07bca17a95fb66d4739a2
Chhayhong <chhayhong@Chhayhongs-MacBook-Pro.local>
2025-02-25 11:03:49 +0700

    Material UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,16 @@
 import { Routes } from '@angular/router';
 import { DetailComponent } from './post/detail/detail.component';
 import { HomeComponent } from './post/home/home.component';
+import { RouteLayerComponent } from './material/route-layer/route-layer.component';
 
 export const routes: Routes = [
-    {
-        path: 'home', component: HomeComponent,children: [
-            {path: ':id',component:DetailComponent}
-        ]
-    },
-    { path: '', redirectTo: '/home', pathMatch: 'full' }
+  {
+    path: 'home',
+    component: HomeComponent,
+    children: [{ path: ':id', component: DetailComponent }],
+  },
+  { path: 'material', component: RouteLayerComponent,children:[
+    { path: 'module', loadChildren: () => import('./material/material.router').then(m => m.material_routes) },
+  ] },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
